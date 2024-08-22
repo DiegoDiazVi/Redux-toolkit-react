9c013b237316fb1215db344dda492eca3ac23ea7
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-22 18:40:15 -0500

    feat: Add counterSlice to Redux store configuration

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slices/counterSlices';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
