8934ef09fd716a681fd8db5db833351cb8893a32
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-22 20:01:41 -0500

    feat: Add decrement action to counterSlice

--- src/store/slices/counterSlices.ts
+++ src/store/slices/counterSlices.ts
@@ -15,8 +15,14 @@ export const counterSlice = createSlice({
         value: state.value + 1,
       };
     },
+    decrement: (state) => {
+      return {
+        ...state,
+        value: state.value - 1,
+      };
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
 export default counterSlice.reducer;
