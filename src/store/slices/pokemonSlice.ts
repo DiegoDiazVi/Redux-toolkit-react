985a869f97854e26c505ff1aa09f0e03219ac371
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 18:05:03 -0500

    feat: Add pokemonSlice to manage Pokemon state

---
+++ src/store/slices/pokemonSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState,
+  reducers: {},
+});
+
+export const {} = pokemonSlice.actions;
+export default pokemonSlice.reducer;
