d4dab27d8c687711e1adfc999c9e2ad735704045
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 18:47:57 -0500

    feat: Update getPokemonsThunk to include type annotations

--- src/store/slices/pokemonThunks.ts
+++ src/store/slices/pokemonThunks.ts
@@ -1,7 +1,9 @@
+import type { Dispatch, GetState } from '@reduxjs/toolkit';
 import { startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemonsThunk = (page: number = 0) => {
-  return async (dispatch, getState) => {
+  return async (dispatch: Dispatch, getState: GetState) => {
+    console.log('Dispatch !!!!');
     dispatch(startLoadingPokemons());
   };
 };
