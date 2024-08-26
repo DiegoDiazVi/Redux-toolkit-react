7319048fb761a0b1103e11bbb9d10cfcc6d4ce4c
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 18:47:39 -0500

    feat: Add dependency on useEffect

--- src/PokemonApp.tsx
+++ src/PokemonApp.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
+import { useActionsPokemons } from './hooks/useActionsPokemons';
 
 export const PokemonApp = (): JSX.Element => {
+  const { getPokemon } = useActionsPokemons();
+
+  useEffect(() => {
+    getPokemon();
+  }, [getPokemon]);
+
   return (
     <>
       <h2>Pokemon App</h2>
