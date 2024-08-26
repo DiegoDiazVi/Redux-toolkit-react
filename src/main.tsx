d916d2ac369b00eac1af060f3670499bfe0e1a8e
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 18:00:21 -0500

    feat: Update main.tsx to use PokemonApp component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.tsx';
 import './index.css';
+import { PokemonApp } from './PokemonApp.tsx';
 import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </StrictMode>
 );
