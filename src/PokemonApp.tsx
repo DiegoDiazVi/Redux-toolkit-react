679d70b160c373e2b00713fc458b9ab9ffbbae57
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 18:00:16 -0500

    feat: Add PokemonApp component

---
+++ src/PokemonApp.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+export const PokemonApp = (): JSX.Element => {
+  return (
+    <>
+      <h2>Pokemon App</h2>
+      <hr />
+
+      <ul>
+        <li>hola</li>
+        <li>hola</li>
+        <li>hola</li>
+      </ul>
+    </>
+  );
+};
