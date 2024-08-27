7852ec985dfbe776cdbf50225c5f32d7d53b9a57
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 19:12:19 -0500

    feat: Fetch and set Pokemon data in getPokemonsThunk

--- src/store/slices/pokemonThunks.ts
+++ src/store/slices/pokemonThunks.ts
@@ -1,9 +1,19 @@
 import type { Dispatch, GetState } from '@reduxjs/toolkit';
-import { startLoadingPokemons } from './pokemonSlice';
+import { Pokemons } from '../../types';
+import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemonsThunk = (page: number = 0) => {
   return async (dispatch: Dispatch, getState: GetState) => {
-    console.log('Dispatch !!!!');
     dispatch(startLoadingPokemons());
+    try {
+      const response = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+      );
+      const data: Pokemons = await response.json();
+      const { results } = data;
+      dispatch(setPokemons(results));
+    } catch (error) {
+      throw new Error(error as string);
+    }
   };
 };
