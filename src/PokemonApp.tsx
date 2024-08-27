9381c41db6b6fe68931d071c997adc1abd1a3c7f
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-26 20:11:59 -0500

    feat: Add pagination to PokemonApp and improve loading state

--- src/PokemonApp.tsx
+++ src/PokemonApp.tsx
@@ -1,9 +1,18 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useActionsPokemons } from './hooks/useActionsPokemons';
+import { useAppSelector } from './hooks/useStore';
 
 export const PokemonApp = (): JSX.Element => {
   const { getPokemon } = useActionsPokemons();
+  const pokemonsState = useAppSelector((state) => state.pokemon);
+  const { page, isLoading, pokemons } = pokemonsState;
+
+  const loading = isLoading === true;
+
+  const handleClick = () => {
+    getPokemon(page);
+  };
 
   useEffect(() => {
     getPokemon();
@@ -14,11 +23,16 @@ export const PokemonApp = (): JSX.Element => {
       <h2>Pokemon App</h2>
       <hr />
 
+      <p>Cargando: {loading}</p>
       <ul>
         <li>hola</li>
         <li>hola</li>
         <li>hola</li>
       </ul>
+
+      <button disabled={loading} onClick={handleClick}>
+        Next page
+      </button>
     </>
   );
 };
